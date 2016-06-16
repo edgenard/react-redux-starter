137020b613a531321d665ca848da653f6ec2c98e
Emmanuel Genard <edgenard@gmail.com>
2016-06-16 11:12:42 -0400

    attempted tests but got error about json need json loader

--- tests/js/test.spec.js
+++ tests/js/test.spec.js
@@ -1,8 +1,14 @@
 import {assert} from 'chai'
-import {add} from '../../src/js/test.js'
+import {add, App} from '../../src/js/test.js'
+import {shallow} from 'enzyme'
 
 describe('Practice Test', () => {
   it('works', () => {
     assert.equal(add(3, 4), 7)
+  })
+  it('Component test', () => {
+    const wrapper = shallow(<App />)
+
+    assert.isTrue(wrapper.contains(<h1>Hello World!</h1>))
   })
 })
